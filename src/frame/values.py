b4557ce21c08f536982b6734d0885f20a1d839a1
scott carver <scott@artificia.org>
2026-02-09 14:24:13 +0100

    Remove duplicates

--- src/frame/values.py
+++ src/frame/values.py
@@ -162,44 +162,6 @@ class Tail(ValueBase, name="tail"):
 
         return self.last_value
 
-class FileReader(ValueBase, name="file"):
-    def __init__(self, settings):
-        settings["renderer"] = settings.get("renderer", "string")
-        super().__init__(settings)
-
-        self.path = Path(settings["path"]).expanduser().absolute()
-        self.mod_time = 0
-        self.last_value = ""
-
-    async def get(self):
-        mod_time = os.path.getmtime(self.path)
-
-        if mod_time > self.mod_time:
-            self.mod_time = mod_time
-            with open(self.path, "r") as f:
-                self.last_value = f.read();
-    
-        return self.last_value
-
-class FileReader(ValueBase, name="file"):
-    def __init__(self, settings):
-        settings["renderer"] = settings.get("renderer", "string")
-        super().__init__(settings)
-
-        self.path = Path(settings["path"]).expanduser().absolute()
-        self.mod_time = 0
-        self.last_value = ""
-
-    async def get(self):
-        mod_time = os.path.getmtime(self.path)
-
-        if mod_time > self.mod_time:
-            self.mod_time = mod_time
-            with open(self.path, "r") as f:
-                self.last_value = f.read();
-    
-        return self.last_value
-
 
 class FileReader(ValueBase, name="file"):
     def __init__(self, settings):
